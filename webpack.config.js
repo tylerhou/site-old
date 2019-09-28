051ca47f667dafa58d432d6739d9cd3c5c1e0b3c
Tyler Hou <tylerhou@google.com>
2019-09-28 03:31:14 -0700

    Upgrade node modules and hugo.

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,29 @@
-let path = require('path');
-let ExtractTextPlugin = require('extract-text-webpack-plugin');
-let ManifestPlugin = require('webpack-manifest-plugin');
+let path = require("path");
+let MiniCssExtractPlugin = require("mini-css-extract-plugin");
+let ManifestPlugin = require("webpack-manifest-plugin");
 
 module.exports = {
   entry: "./src/index.js",
   output: {
     filename: "js/bundle.[hash].js",
-    path: path.resolve(__dirname, 'static', 'assets'),
+    path: path.resolve(__dirname, "static", "assets")
   },
   module: {
     rules: [
       {
         test: /\.scss$/,
-        use: ExtractTextPlugin.extract({
-          use: ['css-loader', 'sass-loader'],
-        })
-      },
-    ],
+        use: [
+          { loader: MiniCssExtractPlugin.loader },
+          "css-loader",
+          "sass-loader"
+        ]
+      }
+    ]
   },
   plugins: [
-    new ExtractTextPlugin("css/styles.[hash].css"),
+    new MiniCssExtractPlugin("css/styles.[hash].css"),
     new ManifestPlugin({
-      fileName: "../../data/manifest.json",
-    }),
+      fileName: "../../data/manifest.json"
+    })
   ]
 };
